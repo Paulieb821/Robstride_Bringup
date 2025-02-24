a4684010fe6c2505d057c60f2176c7f0fda51500
Paul Barsa <pbarsa821@gmail.com>
2025-02-24 14:59:53 -0500

    Experiments with Dynamics Comp and Feedforward

--- motor_testing_control.py
+++ motor_testing_control.py
@@ -7,6 +7,7 @@ import threading
 import matplotlib.pyplot as plt
 import numpy as np
 from PID_gain_calculator import pid_gains
+from PD_gain_calculator import pd_gains
 from actuator import RobstrideActuator, RobstrideActuatorConfig, RobstrideActuatorCommand, RobstrideConfigureRequest
 
 # Cleanup
@@ -49,12 +50,20 @@ b0 = 152.51
 a1 = 0.66
 a0 = 0.0
 
+# Motor parameters
+J = 1/b0
+c = a1/b0
+
 # Poles and time step
-lambda_val = 15
-T = 0.02
+lambda_val = 10
+T = 0.01
+manual_correction = np.array([1.0, 3.0, 1.0])
 
 # DT Control Parameters
-K, L, Ad, Bd, Cd = pid_gains(lambda_val, T, b1, b0, a1, a0)
+#K, L, Ad, Bd, Cd = pid_gains(lambda_val, T, b1, b0, a1, a0)
+#K, L, Ad, Bd, Cd = pid_gains(lambda_val, T, 0.0, 1.0, 0.0, 0.0)
+K, L, Ad, Bd, Cd = pd_gains(lambda_val, T, 0.0, 1.0, 0.0, 0.0)
+#K, L, Ad, Bd, Cd = pd_gains(lambda_val, T, b1, b0, a1, a0)
 K = K[0]
 L = L[0]
 
@@ -63,33 +72,54 @@ motor_ids = [1, 7, 3]
 data = {motor_id: {"time": [], "position": [], "velocity": [], "torque": [], "pos_ref":[], "vel_ref":[], "vel_pred":[], "pos_pred":[]} for motor_id in motor_ids}
 
 # Trajectorty Generation
-def cubic_trajectory(xo, xf, T, time):
-    if time < T:
+def cubic_trajectory(xo, xf, tf, time):
+    if time < tf:
         c0 = xo
-        c2 = 3*(xf - xo)/pow(T,2)
-        c3 = -2*(xf - xo)/pow(T,3)
-        return (c0 + c2*pow(time,2) + c3*pow(time,3), 2*c2*time + 3*c3*pow(time,2))
+        c2 = 3*(xf - xo)/pow(tf,2)
+        c3 = -2*(xf - xo)/pow(tf,3)
+        #return (c0 + c2*pow(time,2) + c3*pow(time,3), 2*c2*time + 3*c3*pow(time,2), 2*c2 + 6*c3*time)
+        return c0 + c2*pow(time,2) + c3*pow(time,3)
     else:
-        return (xf, 0)
+        #return (xf, 0, 0)
+        return xf
 
-def quintic_trajectory(xo, xf, T, time):
+def quintic_trajectory(xo, xf, tf, time):
     # Calculate the coefficients for the quintic polynomial
     a0 = xo
     a1 = 0  # initial velocity is 0
     a2 = 0  # initial acceleration is 0
-    a3 = (10 * (xf - xo)) / (T ** 3)
-    a4 = (-15 * (xf - xo)) / (T ** 4)
-    a5 = (6 * (xf - xo)) / (T ** 5)
-    
-    if time < T:
+    a3 = (10 * (xf - xo)) / (tf ** 3)
+    a4 = (-15 * (xf - xo)) / (tf ** 4)
+    a5 = (6 * (xf - xo)) / (tf ** 5) 
+
+    if time < tf:
         # Compute position and velocity at a given time
         position = a0 + a1 * time + a2 * time**2 + a3 * time**3 + a4 * time**4 + a5 * time**5
         velocity = a1 + 2 * a2 * time + 3 * a3 * time**2 + 4 * a4 * time**3 + 5 * a5 * time**4
-        return position, velocity
+        accel = 2*a2 + 6*a3*time + 12*a4*time**2 + 20*a5*time**3
+        #return position, velocity, accel
+        return position
     else:
         # At the final time, return the final position and velocity = 0
-        return xf, 0
+        #return xf, 0,0 
+        return xf
 
+def generateReferences(x0, xf, move_time, time_step, max_time):
+    pos_ref = np.array([quintic_trajectory(x0, xf, move_time, t) for t in np.linspace(0, max_time, int(max_time/time_step))])
+    numPoints = np.size(pos_ref)
+    vel_ref = np.zeros(numPoints)
+    acc_ref = np.zeros(numPoints)
+    # Useful constants
+    pps = 1/time_step
+    a = -2*pps
+    b = 2*pow(pps,2)
+    T = time_step
+    # Solve for velocity and force references
+    for i in range(numPoints-1):
+        acc_ref[i] = a*vel_ref[i] + b*(pos_ref[i+1] - pos_ref[i])
+        vel_ref[i+1] = vel_ref[i] + T*acc_ref[i]
+    # Add to Trajectory 
+    return pos_ref, vel_ref, acc_ref
 
 # Misc
 max_time = move_time + 3
@@ -103,6 +133,10 @@ def control_thread():
     xhat = np.array([[initial_pos], [initial_vel]])
     # Integrator setup
     sigma = 0
+    # Get references
+    pos_ref_traj, vel_ref_traj, acc_ref_traj = generateReferences(initial_pos, setpoint, move_time, T, max_time)
+    # Step Counter
+    step_cnt = 0
     # Control loop
     while True:
         # Get elapsed time and check for shutdown
@@ -115,7 +149,16 @@ def control_thread():
             supervisor.disable(3)
             break
         # Get trajectory references
-        pos_ref, vel_ref = quintic_trajectory(initial_pos, setpoint, move_time, elapsed)
+        #pos_ref, vel_ref, acc_ref = quintic_trajectory(initial_pos, setpoint, move_time, elapsed)
+        if step_cnt < len(pos_ref_traj):
+            pos_ref = pos_ref_traj[step_cnt]
+            vel_ref = vel_ref_traj[step_cnt]
+            acc_ref = acc_ref_traj[step_cnt]
+            step_cnt += 1
+        else:
+            pos_ref = pos_ref_traj[-1]
+            vel_ref = vel_ref_traj[-1]
+            acc_ref = acc_ref_traj[-1]
         # Get motor state
         state = supervisor.get_actuators_state([1])
         if state:
@@ -123,10 +166,11 @@ def control_thread():
             pos = math.radians(state[0].position)
             vel = math.radians(state[0].velocity)
             # Calculate torque using PD controller
-            cmd_trq = K[0]*(pos_ref-xhat[0,0]) + K[1]*(vel_ref-xhat[1,0]) - K[2]*sigma
+            cmd_acc = acc_ref# + K[0]*(pos_ref-xhat[0,0]) + K[1]*(vel_ref-xhat[1,0])# - K[2]*sigma
+            cmd_trq = J*cmd_acc + 1.8*c*xhat[1,0]
             # Update Estimator and Integrator
-            xhat = Ad @ xhat + Bd * cmd_trq - L * (Cd @ xhat - pos)
-            sigma = sigma + T*(pos - pos_ref)
+            xhat = Ad @ xhat + Bd * cmd_acc - L * (Cd @ xhat - pos)
+            #sigma = sigma + T*(pos - pos_ref)
             #Position Limits
             if abs(pos) > 3.1:
                 cmd_trq = 0
@@ -147,7 +191,7 @@ def control_thread():
         elapsed_end = time.time() - start_time
         cycle_time = elapsed_end - elapsed
 
-        time.sleep(0.019 - cycle_time)
+        time.sleep(0.0095 - cycle_time)
 
 control = threading.Thread(target=control_thread)
 control.start() 
